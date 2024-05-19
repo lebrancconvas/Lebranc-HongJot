c61d43b072dc9cde1f7ae671d75cbd72cd3c56e8
lebrancconvas <lebrancconvas@gmail.com>
2024-05-20 01:53:48 +0700

    ✨ feat: environment load and set flag

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/lebrancconvas/Lebranc-HongJot/db"
 	"github.com/lebrancconvas/Lebranc-HongJot/server"
 )
 
+func Init() {
+
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	flag.Usage = func() {
+		fmt.Println("Usage: server -e {mode}")
+		os.Exit(1)
+	}
+
+	flag.Parse()
+
 	// Init Database
 	db.Init()
 
 	// Start Server
 	server.Start()
+
 }
