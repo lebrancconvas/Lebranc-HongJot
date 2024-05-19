4b507d2f46696660af311255c6633e5507e31f0c
lebrancconvas <lebrancconvas@gmail.com>
2024-05-20 01:05:21 +0700

    ✨ feat: update start server

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"github.com/lebrancconvas/Lebranc-HongJot/db"
+	"github.com/lebrancconvas/Lebranc-HongJot/server"
+)
+
+func main() {
+	// Init Database
+	db.Init()
+
+	// Start Server
+	server.Start()
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,30 @@
 package server
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func Start() {
+	router := NewRouter()
+	go router.Logger.Fatal(router.Start(":" + os.Getenv("PORT")))
+
+	// Graceful Shutdown
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	<- shutdown
+	fmt.Println("\nServer is shutting down...")
+
+	context, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	err := router.Shutdown(context)
+	if err != nil {
+		router.Logger.Fatal(err)
+	}
+}
