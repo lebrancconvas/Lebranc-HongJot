2f4580fef30e5016b145f8887fb30f4e2dd51f90
lebrancconvas <lebrancconvas@gmail.com>
2024-05-20 01:45:44 +0700

    ✨ feat: api authentication

--- utils/auth.go
+++ utils/auth.go
@@ -1,1 +1,18 @@
 package utils
+
+import (
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func Authenticated(username, password string, c echo.Context) (bool, error) {
+	adminUsername := os.Getenv("ADMIN_USERNAME")
+	adminPassword := os.Getenv("ADMIN_PASSWORD")
+
+	if username == adminUsername && password == adminPassword {
+		return true, nil
+	}
+
+	return false, nil
+}
